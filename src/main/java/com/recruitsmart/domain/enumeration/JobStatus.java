690e29304b2f5c118a916b9532d6b742ac6404e4
josephhatton <lucivar.gamer@gmail.com>
2018-06-10 12:00:43 -0500

    Hook up the data

--- src/main/java/com/recruitsmart/domain/enumeration/JobStatus.java
+++ src/main/java/com/recruitsmart/domain/enumeration/JobStatus.java
@@ -4,5 +4,20 @@ package com.recruitsmart.domain.enumeration;
  * The JobStatus enumeration.
  */
 public enum JobStatus {
-    OPEN, FILLED_BY_US, ON_HOLD, FILLED_BY_COMPETITION, CANCELLED
+    OPEN("Open"),
+    FILLED_BY_US("Filled by Us"),
+    ON_HOLD("On Hold"),
+    FILLED_BY_COMPETITION("Filled By Competition"),
+    CANCELLED("Cancelled");
+
+    private String jobStatus;
+    private JobStatus(String jobStatus) {
+        this.jobStatus = jobStatus;
+    }
+
+    @Override
+    public String toString(){
+        return jobStatus;
+    }
+
 }
